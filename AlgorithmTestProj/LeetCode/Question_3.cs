3669f3c4aef5ae9a772f4ba12b7cf27db09cc4b0
aiol <aiol_cn@163.com>
2022-03-19 14:04:50 +0800

    Q3更改

--- AlgorithmTestProj/LeetCode/Question_3.cs
+++ AlgorithmTestProj/LeetCode/Question_3.cs
@@ -6,21 +6,37 @@ using Xunit;
 namespace AlgorithmTestProj.LeetCode
 {
     /// <summary>
-    /// 第三题 ： 无重复的最长子串
+    /// 第三题：无重复的最长子串
     /// </summary>
     public class Question_3
     {
+        #region - 问题 -
+        /*   描述：
+         *   
+         *   给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+         *   
+         *   示例：
+         *   
+         *   输入: s = "abcabcbb"
+         *   输出: 3 
+         *   解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+         *   
+         */
+        #endregion
+
+        #region - 样例 -
         // private static string s = "abcabcbb";
         // private static string s = "pwwkew";
         private static string s = "wobgrovw";
         // private static string s = " ";
+        #endregion
 
-        #region 解法 1.0
+        #region - 解法 -
 
+        #region 解法1
         [Fact]
         public void LengthOfLongestSubstring()
         {
-
             var res = 0;
             var len = s.Length;
             List<char> list = new List<char>();
@@ -57,7 +73,9 @@ namespace AlgorithmTestProj.LeetCode
             }
             Assert.Equal(6, res);
         }
-
+        #endregion
+        // 执行用时：156 ms, 在所有 C# 提交中击败了 19.01% 的用户
+        // 内存消耗：60.6 MB, 在所有 C# 提交中击败了 5.14% 的用户
 
         #endregion
 
